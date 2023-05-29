122d154887e5516e937eaf8b98eb35d2aff5b1eb
Patryk <wierzbowskipatryk0@gmail.com>
2023-05-29 17:15:45 +0200

    Add create and models data

--- CVMakerApp/models.py
+++ CVMakerApp/models.py
@@ -1,3 +1,43 @@
+from random import choices
 from django.db import models
 
 # Create your models here.
+
+
+
+class Characteristic(models.Model):
+    name = models.CharField(max_length=200)
+
+class Skill(models.Model):
+    name = models.CharField(max_length=200)
+    level_choices = models.IntegerChoices('Level', [('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5')])
+
+class Language(models.Model):
+    name = models.CharField(max_length=200)
+    level_choices = models.IntegerChoices('Level', [('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5')])
+
+class Education(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=400)
+    date = models.DateField()
+
+class Experience(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=400)
+    date = models.DateField()
+
+
+
+class Data(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    mail = models.EmailField(max_length=100)
+    image = models.ImageField(upload_to='uploads/', blank=False)
+    phone = models.SmallIntegerField(max_length=20)
+    aboutMe = models.TextField(max_length=2500)
+    characteristics = models.ForeignKey(Characteristic, on_delete=models.CASCADE)
+    skills = models.ForeignKey(Skill, on_delete=models.CASCADE)
+    languages = models.ForeignKey(Language, on_delete=models.CASCADE)
+    education = models.ForeignKey(Education, on_delete=models.CASCADE)
+    experience = models.ForeignKey(Experience, on_delete=models.CASCADE)
+
--- CVMakerApp/views.py
+++ CVMakerApp/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def create(request, type):
+    return render(request, 'create.html')
